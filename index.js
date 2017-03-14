9042b93da86621a2104fd22a343563ea79d68075
turbopope <mbrack@uni-koblenz.de>
2017-03-14 09:49:30 +0100

    Make path to idman absolute

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const exec = require('child_process').execSync;
+const path = require('path');
 
 module.exports = function(repo, algorithm = 'default', ...args) {
-  const stdout = exec(`./idman ${repo} ${algorithm} ${args.join(' ')}`, { encoding: 'utf-8' });
+  const stdout = exec(`${__dirname}${path.sep}idman ${repo} ${algorithm} ${args.join(' ')}`, { encoding: 'utf-8' });
   return JSON.parse(stdout);
 }
